f9180a14cc96ecc518eb10aba33095dc348a576b
Nikula <simo.nikula@capgemini.com>
2019-11-24 21:45:17 +0200

    ifs.m introduced to provide impulse from string

--- src/sie_e.m
+++ src/sie_e.m
@@ -6,35 +6,36 @@ t = 0.0;
 dt = 0.1;
 velocity = 0.0;
 position = 0;
-k=300;
-g=10;
+k=1;
+g=-10;
 force = 0;
 mass = 1.0;
 fprintf('%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s\n',
   'time','position','ap','velocity','energy',
   'elastic-e','kinetic-e','potential-e');
 i=1;
-e0=0.5*k*position*position+mass*g*position;
+e0=0.5*k*position*position-mass*g*position;
 clear v[tpve];
-while t <= 20.1*2*pi*sqrt(mass/k)
+while t <= 1.1*2*pi*sqrt(mass/k)
   % store and print results 
   vt(i)=t;
   vp(i)=position;
   vv(i)=velocity;
   ee=0.5*k*position*position;
   ek=0.5*mass*velocity*velocity;
-  ep=mass*g*position;
+  ep=-mass*g*position;
   e=ee+ek+ep;
   ve(i)=e;
   ap=(position+velocity*dt/2);
   fprintf('%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f\n',
     t,position,ap,velocity,e,ee,ek,ep);
-  % forceFromElasticEnergy - better than -k*position
-  % position at middle of timestep creates damping
+  % Is impulse from spring - better than -k*position*dt
+  % - position at middle of timestep creates damping and is not stable
   %
-  ffee=-k*ap; 
-  force=ffee-mass*g;
-  velocity +=( force / mass ) * dt;
+  Is=ifs(k,mass,position,velocity,dt,0); 
+  Ig=mass*g*dt;
+  I=Ig+Is;
+  velocity += I/mass;
   position += velocity * dt;
   i++;
   t += dt;
@@ -42,7 +43,7 @@ while t <= 20.1*2*pi*sqrt(mass/k)
  clf
  subplot(1,3,1);
  plot(vt,vp,'k');
- title(["position [m]  " datestr(now())]);
+ title(["e - position [m]  " datestr(now())]);
  subplot(1,3,2);
  plot(vt,vv,'r');
  title(['velocity [m/s] - k=' num2str(k)]);
--- src/sie_wpe.m
+++ src/sie_wpe.m
@@ -6,20 +6,20 @@ dt = 0.1;
 velocity = 0.0;
 position = 0;
 k=1;
-g=10;
+g=-10;
 force = 0;
 mass = 1.0;
 fprintf('%-12s%-12s%-12s%-12s\n','time','position','velocity','energy');
 i=1;
-e0=0.5*k*position*position+mass*g*position;
+e0=0.5*k*position*position-mass*g*position;
 clear v[tpve];
 while t <= 1.1*2*pi*sqrt(mass/k)
   vt(i)=t;
   vp(i)=position;
   vv(i)=velocity;
-  e=0.5*k*position*position+0.5*mass*velocity*velocity+mass*g*position;
+  e=0.5*k*position*position+0.5*mass*velocity*velocity-mass*g*position;
   ve(i)=e;
-  force=-k*position-mass*g;
+  force=-k*position+mass*g;
   velocity +=( force / mass ) * dt;
   position += velocity * dt;
   fprintf('%-12.3f%-12.3f%-12.3f%-12.3f\n',t,position,velocity,e);
@@ -29,7 +29,7 @@ while t <= 1.1*2*pi*sqrt(mass/k)
  clf
  subplot(1,3,1);
  plot(vt,vp,'k');
- title(["position [m]  " datestr(now())]);
+ title(["wpe - position [m]  " datestr(now())]);
  subplot(1,3,2);
  plot(vt,vv,'r');
  title('velocity [m/s]');

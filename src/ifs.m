96703e886c4cf12ce6d087e09c5fbdaabb57fcb5
Nikula <simo.nikula@capgemini.com>
2019-12-06 22:32:10 +0200

    Attempt to integrate position during impulse started

--- src/ifs.m
+++ src/ifs.m
@@ -1,15 +1,28 @@
 %
-% type 0:
+%
+function v=ifs(k,mass,position,velocity,dt,w0,vmax,pmax,pmin,type)
+  switch(type)
+  case 1
 % spring force at start of step for whole step
 % - causes too large impulse if start position
-%   is near maximum and step is large
-% type 1:
-%
-%
-function v=ifs(k,mass,position,velocity,dt,w0,type)
-  switch(type)
-  case 0
+%   is near maximum and step is large    
     v=-k*position*dt;
+case 2
+% integrate -k*position
+% position is taken as position+sin(x)*vmax*dt
+% int(sin(x))=-cos(x)
+% so that initial position and velocity match
+% 
+    v=-k*position*dt;
+    if(vmax>sqrt(eps))
+      xs=asin(velocity/vmax);
+      xe=xs+dt*w0;
+      is=-cos(xs);
+      ie=-cos(xe);
+      d=vmax*(ie-is);
+      r=abs(d/v);
+      v+=d;
+    endif
   otherwise
   error(["type " num2str(type) " not supported in ifs"]);
   endswitch
--- src/sie_e.m
+++ src/sie_e.m
@@ -1,42 +1,57 @@
 %
 % vertical energy based v. 1
-% gravity and potential energy are taken into account
+% gravity and potential energy are taken into account if g is set
 % 
 t = 0.0;
 dt = 0.1;
-velocity = 0.0;
-position = 0;
 k=1;
-g=-10;
-force = 0;
+g=0;
 mass = 1.0;
-fprintf('%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s\n',
-  'time','position','ap','velocity','energy',
-  'elastic-e','kinetic-e','potential-e');
+v0=sqrt(0.01/2);
+p0=sqrt(0.01/2);
+pg0=-mass*g/k;
+fprintf('%-11s%-11s%-11s%-11s%-11s%-11s%-11s%-11s\n',
+  'time','position','velocity','energy',
+  'elastic-e','kinetic-e','potential-e','ifs');
 i=1;
-e0=0.5*k*position*position-mass*g*position;
-clear v[tpve];
+[e0,ee,ek,ep] = energy(p0,v0,mass,k,g);
+pa=sqrt(2*(ee+ek)/k);
+vmax=sqrt(2*(ee+ek)/mass);
 w0=sqrt(k/mass);
+pf=asin(p0/w0);
+clear v[tpvem];
+clear i[s];
+clear t[vp];
+velocity=v0;
+position=p0;
+tvs=1;
+vmax=velocity;
+pmax=position;
+pmin=position;
 if(w0*dt>2)
   fprintf('Integration will be unstable w0*dt= %f, i.e.>2\n',w0*dt);
 endif
-while t <= 2.1*2*pi/w0
-  % store and print results 
+ifsm=1;
+ifsmt={"-k*p*dt","-k*int(p*dt)"};
+while t <= 1.1*2*pi/w0
+  vmax=max(vmax,velocity);
+  pmax=max(pmax,position);
+  pmin=min(pmin,position);
+  % store for printing results 
   vt(i)=t;
   vp(i)=position;
+  tp(i)=pg0+sin(w0*t+pf)*pa;
+  tv(i)=cos(w0*t+pf)*vmax;
   vv(i)=velocity;
-  ee=0.5*k*position*position;
-  ek=0.5*mass*velocity*velocity;
-  ep=-mass*g*position;
-  e=ee+ek+ep;
-  ve(i)=e;
-  ap=(position+velocity*dt/2);
-  fprintf('%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f%-12.3f\n',
-    t,position,ap,velocity,e,ee,ek,ep);
+  ve(i)=energy(position,velocity,mass,k,g);
+  te(i)=energy(tp(i),tv(i),mass,k,g);
+  Is=ifs(k,mass,position,velocity,dt,w0,vmax,pmax,pmin,ifsm); 
+  fprintf('%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g\n',
+    t,position,velocity,e,ee,ek,ep,Is);
   % Is impulse from spring - better than -k*position*dt
   % - position at middle of timestep creates damping and is not stable
   %
-  Is=ifs(k,mass,position,velocity,dt,w0,0); 
+  is(i)=Is;
   Ig=mass*g*dt;
   I=Ig+Is;
   velocity += I/mass;
@@ -45,12 +60,18 @@ while t <= 2.1*2*pi/w0
   t += dt;
  end 
  clf
- subplot(1,3,1);
- plot(vt,vp,'k');
- title(["e - position [m]  " datestr(now())]);
- subplot(1,3,2);
- plot(vt,vv,'r');
- title(['velocity [m/s] - k=' num2str(k)]);
- subplot(1,3,3);
- plot(vt,ve,'b');
- title(['energy [J] - \omega\Delta_t=' num2str(w0*dt)]);
+ subplot(1,5,1);
+ plot(vt,vp,'k',vt,tp,'r');
+ title(["sie: p [m]  " datestr(now())]);
+ subplot(1,5,2);
+ plot(vt,vv,'k',vt,tv,'r');
+ title(['v [m/s] - k=' num2str(k)]);
+ subplot(1,5,3);
+ plot(vt,ve,'k',vt,te,'r');
+ title(['e [J] - \omega\Delta_t=' num2str(w0*dt)]);
+ subplot(1,5,4);
+ plot(vt,is,'b');
+ title(['ifs [Ns] = ' ifsmt{ifsm}]);
+% subplot(1,5,5);
+% plot(vt,vm,'b');
+% title(['v_{max} [m/s]']);

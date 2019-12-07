efc291ee2286d0fbdc2113f326fca845a5c41743
Nikula <simo.nikula@capgemini.com>
2019-12-07 23:35:43 +0200

    Integrate position during impulse cleanup

--- src/ifs.m
+++ src/ifs.m
@@ -9,16 +9,16 @@ function i=ifs(k,m,p,v,dt,w0,pg0,pa,type)
     i=-k*p*dt;
   case 2
 % integrate -k*position
-% position is taken as position+sin(x+pf)*vmax*dt
-% int(sin(x))=-cos(x+pf)
-% so that initial position and velocity match
+% position is taken as pg0+sin(x+pf)*pa
+% int(sin(x))=-cos(x)
+% pf (phase fix) so that initial position matches
 % 
-    pf=asin((p-pg0)/pa)
+    pf=asin((p-pg0)/pa);
     i0=pg0*dt;
-    ie=-cos(w0*dt+pf)
-    is=-cos(pf)
-    id=ie-is
-    i=-k*(i0+pa*id)
+    ie=-cos(w0*dt+pf);
+    is=-cos(pf);
+    id=ie-is;
+    i=-k*(i0+pa*id);
   otherwise
     error(["type " num2str(type) " not supported in ifs"]);
   endswitch

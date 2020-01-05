a7fcf809b7670ebac5a2b771771f14d881e103e0
Nikula <simo.nikula@capgemini.com>
2020-01-05 18:50:10 +0200

    theoretical calculation initialization fixes

--- src/sie_e.m
+++ src/sie_e.m
@@ -11,11 +11,11 @@ pg0=-mass*g/k
 v0=sqrt(0.01/2);
 p0=pg0+sqrt(0.01/2);
 v0=0;
-p0=-11;
+p0=0;
 i=1;
-[e0_pg0,ee_pg0,ek_pg0,ep_pg0]=energy(pg0,0,mass,k,g)
 [e0,ee,ek,ep] = energy(p0,v0,mass,k,g)
-ea=(ee+ek-(ee_pg0+ek_pg0))
+[e0_pg0,ee_pg0,ek_pg0,ep_pg0]=energy(pg0,0,mass,k,g)
+ea=e0-e0_pg0
 pa=sqrt(2*ea/k)
 va=sqrt(2*ea/mass)
 w0=sqrt(k/mass)
@@ -38,7 +38,7 @@ ig=-mass*g*dt % impulse from gravity
 fprintf('%-11s%-11s%-11s%-11s%-11s%-11s%-11s%-11s\n',
   'time','position','velocity','energy',
   'elastic-e','kinetic-e','gravity-e','ifs');
-while t <= 1.1*2*pi/w0
+while t <= 3.1*2*pi/w0
   % store for printing results 
   vt(i)=t;
   vp(i)=position;
@@ -49,7 +49,7 @@ while t <= 1.1*2*pi/w0
   [ve(i),ee,ek,ep]=energy(position,velocity,mass,k,g);
   te(i)=energy(tp(i),tv(i),mass,k,g);
   Is=ifs(k,mass,position,velocity,dt,w0,pg0,pa,ifsm); 
-  fprintf('%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g%-11.3g\n',
+  fprintf('%-11.4g%-11.4g%-11.4g%-11.4g%-11.4g%-11.4g%-11.4g%-11.4g\n',
     t,position,velocity,ve(i),ee,ek,ep,Is);
   % Is impulse from spring - hopefully better than -k*position*dt
   is(i)=Is;
